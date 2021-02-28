766c25e7144de1c66bf3675b5b606fa90af0aae3
serhiidorodko <serhiidorodko@gmail.com>
2021-02-28 09:53:41 +0200

    Add employee not found handler

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +31,7 @@ public class EmployeeController {
     }
     @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id){
-        return repository.getOne(id);
+        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
     }
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
