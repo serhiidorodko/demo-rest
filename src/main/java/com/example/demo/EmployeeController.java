c9efdbaabcb788640accb2485eea62b135d2833e
serhiidorodko <serhiidorodko@gmail.com>
2021-03-03 10:47:59 +0200

    Add hateoas to employees all() controller

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,6 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 class EmployeeController {
-    //private final static Logger log = LoggerFactory.getLogger(EmployeeController.class);
     private final EmployeeRepository repository;
 
     EmployeeController(EmployeeRepository repository){
@@ -25,8 +24,14 @@ class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all(){
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all(){
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+            .map(employee -> EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+            .collect(Collectors.toList());
+        return CollectionModel.of(employees,
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee){
