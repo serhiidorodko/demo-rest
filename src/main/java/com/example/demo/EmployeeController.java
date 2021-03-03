b645c13bc6602054f13b91b9590e25271862728b
serhiidorodko <serhiidorodko@gmail.com>
2021-03-03 12:07:19 +0200

    Create representation hateoas model for employee

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -18,17 +18,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @RestController
 class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository){
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all(){
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+            .map(assembler::toModel)
             .collect(Collectors.toList());
         return CollectionModel.of(employees,
             linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -40,9 +40,7 @@ class EmployeeController {
     @GetMapping("/employees/{id}")
     EntityModel<Employee> one(@PathVariable Long id){
         Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-        return EntityModel.of(employee,//
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
