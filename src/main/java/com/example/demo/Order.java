4b91002e0e72caab1f98ed4b2170cd9d02339328
serhiidorodko <serhiidorodko@gmail.com>
2021-03-04 15:35:47 +0200

    Add Order entity

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
+public class Order {
+
+    private @Id @GeneratedValue Long  id;
+    private String description;
+    private String status;
+
+    Order(){
+
+    }
+    Order(String description, String status){
+        this.description = description;
+        this.status = status;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(!(this instanceof Order))
+            return false;
+        Order o = (Order) obj;
+        return Objects.equals(this.id, o.id) 
+            && Objects.equals(this.description, o.description) 
+            && Objects.equals(this.status, o.status);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.description, this.status);
+    }
+    @Override
+    public String toString() {
+        return "Order{" +
+            "id=" + this.id +
+            ", description='" + this.description + '\'' +
+            ", status='" + this.status + '\'';
+    }
+
+}
